b62835893440bde2ac37de8fb8b24c9e524e4aac
go4retro <go4retro@go4retro.com>
2019-09-22 00:56:41 -0500

    Adding in MMU config capability

--- hdl/UltiMem64.v
+++ hdl/UltiMem64.v
@@ -31,45 +31,71 @@ module UltiMem64(
                 output _we_ram,
                 output _ub,
                 output _lb,
-                output [4:1]test
+                output [8:1]test
                );
 
 reg [7:0]address;
+wire [15:0]faddress;
+wire [7:0]data_task_win;
+wire [7:0]data_task;
+reg [7:0]data_out;
+wire ce_mmu_regs;
+wire ce_mmu_ctrl;
+wire ce_mmu_task_conf;
+wire ce_mmu_task;
+wire ce_mmu_map;
+wire we;
+wire oe;
+wire flag_mmu_enabled;
+wire clock;
 
-//assign _ce_ram =           (_cas | _ras);
-assign _ce_ram =           1;
-assign _ce_tag =           (_cas | _ras);
-assign _we_ram =           _we;
-assign baddress[20:0] =    {5'b0, maddress, address};
-assign bdata =             ((!_ce_ram | !_ce_tag) & !_we_ram ? data : 8'bz);  // RAM selected and we're writing, else bz
-assign data =              ((!_ce_ram | !_ce_tag) & _we_ram ? bdata : 8'bz);  // RAM selected and we're reading, else bz
-assign _ub =               1;
-assign _lb =               0;
-//assign _lb =               1;
-//assign _ce_tag =           1;
-
-// bit 0 is bit 6.
-// bit 1 is bit 1.
-// bit 2 is bit 2.
-// bit 3 is bit 3.
-// bit 4 is bit 4.
-// bit 5 is bit 5.
-// bit 6 is bit 0.
-// bit 7 is bit 7.
-// bit 8 is bit 14.
-// bit 9 is bit 9.
-// bit 10 is bit 10.
-// bit 11 is bit 11.
-// bit 12 is bit 12.
-// bit 13 is bit 13.
-// bit 14 is bit 8.
-// bit 15 is bit 15.
-
-
-assign test[1] =           (!_ras & _cas);
-assign test[2] =           0;
-assign test[3] =           0;
+assign test[1] =           (!_ras & !_cas);
+assign test[2] =           !_we & (faddress == 49152);
+assign test[3] =           flag_mmu_enabled;
 assign test[4] =           0;
+assign test[5] =           0;
+assign test[6] =           0;
+assign test[7] =           0;
+assign test[8] =           0;
+
+assign faddress =          {maddress, address};
+assign ce_mmu_regs =       (faddress[15:4] == 12'hc00);
+assign ce_mmu_ctrl =       ce_mmu_regs & (faddress[3:0] == 0);
+assign ce_mmu_task_conf =  ce_mmu_regs & (faddress[3:0] == 1);
+assign ce_mmu_task =       ce_mmu_regs & (faddress[3:0] == 2);
+assign ce_mmu_map =        (faddress[15:4] == 12'hc01);
+assign we =                !_we;
+assign oe =                _we;
+assign clock =             !_ras & !_cas;
+
+assign _ce_ram =           !(clock & !ce_mmu_map);
+assign _ce_tag =           !(clock & ce_mmu_map);
+assign _we_ram =           _we;
+assign baddress[20:0] =    (!_ce_ram ? {5'b0, faddress} : {9'b0, data_task_win, faddress[3:0]});
+assign bdata =             ((!_ce_ram | !_ce_tag) & !_we_ram ? data : 8'bz);  // RAM selected and we're writing, else bz
+assign data =              data_out;  // RAM selected and we're reading, else bz
+assign _ub =               1;
+assign _lb =               _ce_tag;
+
+register #(.WIDTH(1))				mmu_enabled_reg(clock, 0, we & ce_mmu_ctrl, data[0], flag_mmu_enabled);
+register #(.WIDTH(8))				mmu_win_reg(clock, 0, we & ce_mmu_task_conf, data, data_task_win);
+register #(.WIDTH(8))				mmu_task_reg(clock, 0, we & ce_mmu_task, data, data_task);
+
+always @(*)
+begin
+   if(ce_mmu_ctrl & oe)
+      data_out = {7'b0,flag_mmu_enabled};
+   else if (ce_mmu_task_conf & oe)
+      data_out = data_task_win;
+   else if (ce_mmu_task & oe)
+      data_out = data_task;
+   else if(!_ce_ram & oe)
+      data_out = bdata;
+   else if (!_ce_tag & oe & !_lb)
+      data_out = bdata;
+   else
+      data_out = 8'bz;
+end
 
 always @(negedge _ras)
 begin
---
+++ hdl/register.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:52:36 12/08/2013 
+// Design Name: 
+// Module Name:    register 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`ifndef _register
+`define _register
+module register(clock, reset, enable, d, q);
+
+parameter WIDTH = 8 ;
+parameter RESET = 0 ;
+
+input clock;
+input reset;
+input enable;
+input [WIDTH-1:0] d;
+output [WIDTH-1:0] q;
+reg [WIDTH-1:0] q;
+initial q = RESET;
+
+always @ (negedge clock, posedge reset)
+  begin
+  if(reset)
+		q <= RESET;
+  else if(enable)
+	   q <= d;
+  end
+endmodule
+`endif
